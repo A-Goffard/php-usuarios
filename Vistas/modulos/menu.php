ebbd11b80374c22a51d9699cd213cc6b0f075040
A-Goffard-Hodeia <aintzane.goffard@hodeia.digital>
2024-08-07 13:27:29 +0200

    base curso php

---
+++ Vistas/modulos/menu.php
@@ -0,0 +1,35 @@
+  <!-- Left side column. contains the sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="vistas/imagenes/usuarios/mariposa.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Aintzane Goffard</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Menú de navegación</li>
+        
+        <li>
+          <a href="usuarios">
+            <i class="fa fa-user"></i><span>Usuarios</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="roles">
+            <i class="fa fa-link"></i><span>Roles</span>
+          </a>
+        </li>
+
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
